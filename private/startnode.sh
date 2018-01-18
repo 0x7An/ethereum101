ab671c0ded6676d252400812e41a76496ba60e8e
AndersonDSL <primataxdeus@gmail.com>
2018-01-18 21:15:40 -0200

    Created the file starnode.sh, to initialize the first node

---
+++ private/startnode.sh
@@ -0,0 +1,1 @@
+geth --networkid 4224 --mine --datadir "~/blockchain/ethereum101/private/" --nodiscover --rpc --rpcport "8545" --port "30303" --rpccorsdomain "*" --nat "any" --rpcapi eth,web3,personal,net --unlock 0 --password ~/blockchain/ethereum101/private/password.sec --ipcpath "~/.ethereum/geth.ipc"                          
